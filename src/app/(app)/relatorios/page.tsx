9054f59f53fdec0dbf48013d65bffc414d144708
Geovani silva de oliveira <geovanisilvadeoliveira447@gmail.com>
2025-07-20 19:57:51 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <button class="inl

--- src/app/(app)/relatorios/page.tsx
+++ src/app/(app)/relatorios/page.tsx
@@ -1,6 +1,7 @@
 import { Card, CardHeader, CardTitle, CardDescription, CardContent } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
 import { TrendingUp, ShoppingCart, Users, Package } from "lucide-react";
+import Link from "next/link";
 
 export default function RelatoriosPage() {
   return (
@@ -12,7 +13,11 @@ export default function RelatoriosPage() {
           <CardDescription>Escolha um dos relatórios abaixo para gerar.</CardDescription>
         </CardHeader>
         <CardContent className="flex flex-wrap gap-4">
-          <Button><TrendingUp className="mr-2 h-4 w-4" /> Relatório de Vendas</Button>
+          <Button asChild>
+            <Link href="/relatorios/vendas">
+              <TrendingUp className="mr-2 h-4 w-4" /> Relatório de Vendas
+            </Link>
+          </Button>
           <Button><ShoppingCart className="mr-2 h-4 w-4" /> Relatório de Compras</Button>
           <Button><Users className="mr-2 h-4 w-4" /> Lista de Funcionários</Button>
           <Button><Package className="mr-2 h-4 w-4" /> Lista de Produtos</Button>
