dabcce52b5ce5dec368b6a0811edecafcc09b778
Geovani silva de oliveira <geovanisilvadeoliveira447@gmail.com>
2025-07-20 01:50:27 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <button class="inl

---
+++ src/types/socio.ts
@@ -0,0 +1,31 @@
+
+export interface Socio {
+  id?: string;
+  // Personal Data
+  nomeCompleto: string;
+  dataNascimento: Date;
+  cpf: string;
+  rg: string;
+  estadoCivil: string;
+  nacionalidade: string;
+  profissao: string;
+  
+  // Address
+  cep: string;
+  logradouro: string;
+  numero: string;
+  complemento?: string;
+  bairro: string;
+  cidade: string;
+  uf: string;
+  
+  // Contact
+  email?: string;
+  telefone: string;
+
+  // Corporate Data
+  dataEntrada: Date;
+  participacao: number;
+  proLabore: number;
+  isAdministrador: boolean;
+}
