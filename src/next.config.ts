4fd4cc00c66f5ab7cb7d9197f0035420e2f86115
Geovani silva de oliveira <geovanisilvadeoliveira447@gmail.com>
2025-08-20 19:28:51 +0000

    The NextJS app server is having trouble starting. Please identify what c

--- src/next.config.ts
+++ src/next.config.ts
@@ -25,25 +25,6 @@ const nextConfig: NextConfig = {
       }
     ],
   },
-  webpack: (config, { isServer }) => {
-    // This is to make pdf-parse work, which is not fully compatible with webpack5
-    // It will be used on the client-side for parsing PDFs.
-    if (!isServer) {
-        config.resolve.fallback = {
-            "fs": false,
-            "path": false,
-            "os": false,
-            "crypto": false,
-            "stream": require.resolve("stream-browserify"),
-            "buffer": require.resolve("buffer/"),
-        }
-    }
-    
-    // This is a workaround for the 'Critical dependency: the request of a dependency is an expression' warning
-    config.module.exprContextCritical = false;
-
-    return config;
-  },
 };
 
 export default nextConfig;
