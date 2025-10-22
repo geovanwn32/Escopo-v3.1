c8a9ceed498c92861c5fb5b597d87348589f6333
Geovani silva de oliveira <geovanisilvadeoliveira447@gmail.com>
2025-10-22 18:12:27 +0000

    Try fixing this error: `Console Error: Error: Failed to fetch from https

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -6,6 +6,6 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 
 export const ai = genkit({
-  plugins: [googleAI()],
+  plugins: [googleAI({apiVersion: 'v1'})],
   model: 'googleai/gemini-pro',
 });
